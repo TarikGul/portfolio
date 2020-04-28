c8bf4118fe0269c5f833d47d65109011158f7de3
Tarik <tariksnow37@gmail.com>
2020-04-28 10:43:34 -0700

    font-size

---
+++ frontend/src/components/main/projects/projects.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Projects = props => {
+    return (
+        <div className='projects-container'>
+            <div className='projects-inner-container'>
+                <div className='bimeo-container'>
+                    Bimeo
+                    <img src="" alt=""/>
+                </div>
+                <div className='covid-container'>
+                    Covid-415
+                    <img src="" alt=""/>
+                </div>
+                <div className='sudoku-container'>
+                    Sudoku-Visualizer
+                    <img src="" alt=""/>
+                </div>
+            </div>
+        </div>
+    )
+}
