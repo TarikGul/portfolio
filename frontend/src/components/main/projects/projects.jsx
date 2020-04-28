28a032ae29c660db4082ec7846b0fc14e42e41a0
Tarik <tariksnow37@gmail.com>
2020-04-28 11:05:54 -0700

    projects

--- frontend/src/components/main/projects/projects.jsx
+++ frontend/src/components/main/projects/projects.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import '../../../styles/projects.scss';
 
 const Projects = props => {
     return (
         <div className='projects-container'>
             <div className='projects-inner-container'>
-                <div className='bimeo-container'>
-                    Bimeo
-                    <img src='/bimeo-page.png' className='bimeo-project'/>
+                <div className='projects-header'>
+                    Projects
                 </div>
-                <div className='covid-container'>
-                    Covid-415
-                    <img src="" alt=""/>
-                </div>
-                <div className='sudoku-container'>
-                    Sudoku-Visualizer
-                    <img src="" alt=""/>
+                <div className='projects-boxes'>
+                    <div className='bimeo-container'>
+                        Bimeo
+                        <img src='/bimeo-page.png' className='image-project'/>
+                    </div>
+                    <div className='covid-container'>
+                        Covid-415
+                        <img src='/covid-415.png' className='image-project'/>
+                    </div>
+                    <div className='sudoku-container'>
+                        Sudoku-Visualizer
+                        <img src="" alt=""/>
+                    </div>
                 </div>
             </div>
         </div>
