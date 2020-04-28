2883b0867c344498c8930e7ce093599c65c27f35
Tarik <tariksnow37@gmail.com>
2020-04-27 18:34:48 -0700

    skills boilerplate

--- frontend/src/components/main/skills/skills.jsx
+++ frontend/src/components/main/skills/skills.jsx
@@ -5,7 +5,10 @@ const Skills = props => {
     return (
         <div className='skills-container'>
             <div className='skills-inner-container'>
-                
+                <div className='node-container'>
+                    <img src='/nodejs-icon.svg' />
+                    Node.js
+                </div>
             </div>
         </div>
     )
