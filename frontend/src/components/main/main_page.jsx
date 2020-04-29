ad128ff95bcdda558205e66938e7cb7a31eada10
Tarik <tariksnow37@gmail.com>
2020-04-28 20:56:51 -0700

    comment out modal

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -14,7 +14,7 @@ class Main extends React.Component {
     componentDidMount() {
         const { openModal } = this.props;
 
-        openModal('welcome-visitor');
+        // openModal('welcome-visitor');
     }
 
     render() {
