c4c4f9bf07f2e8f9323fac97b79390d29a2ee397
Tarik <tariksnow37@gmail.com>
2020-04-28 20:56:13 -0700

    remove hey

--- frontend/src/components/modal/visitor_modal.jsx
+++ frontend/src/components/modal/visitor_modal.jsx
@@ -10,7 +10,7 @@ class VisitorModal extends React.Component {
     render() {
         return (
             <div className='visitor-modal-container'>
-                Heyyyyyyy
+                
             </div>
         )
     };
