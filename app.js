294a24420af3f017829cd775caa40054c54f76e8
Tarik <tariksnow37@gmail.com>
2020-04-28 21:28:28 -0700

    prod

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Load static build folder in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+};
+
 
 const db = require('./config/keys').mongoURI;
 // Connect database using mongoose
