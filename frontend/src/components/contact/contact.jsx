5e50cbc2fe3e4720d5ad51cfc4c8d8df4276129b
Tarik <tariksnow37@gmail.com>
2020-05-01 17:31:38 -0700

    contact component

---
+++ frontend/src/components/contact/contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = props => {
+
+    return (
+        <div className='contact-container'>
+            If you would like to email me please contact me at tariksnow37@gmail.com.
+        </div>
+    )
+}
+
+export default Contact;
