c5ec62d6a1d49cfc5722f92478bd7b785c56213b
Tarik <tariksnow37@gmail.com>
2020-05-10 01:14:16 -0700

    add session

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+
+import session from './session_reducer';
 import ui from './ui_reducer';
 
 const RootReducer = combineReducers({
+    session,
     ui
 });
 
