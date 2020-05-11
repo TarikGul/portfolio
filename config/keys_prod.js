d9807ff34416311cfec24c96fa2795c5fb0ea050
Tarik <tariksnow37@gmail.com>
2020-05-11 00:51:07 -0700

    aws access key

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,6 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
+    awsAccessKey: process.env.AWS_ACCESS_KEY,
     mapboxPublicKey: process.env.MAP_BOX_PUBLIC_KEY
 };
