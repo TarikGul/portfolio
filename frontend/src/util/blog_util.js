f051a995de1bc561e055dc5b605af3795d5303f3
Tarik <tariksnow37@gmail.com>
2020-05-12 15:04:58 -0700

    axios get all blogs

--- frontend/src/util/blog_util.js
+++ frontend/src/util/blog_util.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const createBlog = blogData => {
     return axios.post('/api/blogs/blog', blogData)
 }
+
+export const getBlogs = () => {
+    return axios.get('/api/blogs')
+}
