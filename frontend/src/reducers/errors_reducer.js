3ca630399c0b41b627ed993b7b66bbf5c494fab3
Tarik <tariksnow37@gmail.com>
2020-05-14 16:52:30 -0700

    import blog errors

--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import SessionErrorsReducer from './session_errors_reducer';
+import BlogErrorsReducer from './blog_errors_reducer';
 
 export default combineReducers({
-    session: SessionErrorsReducer
+    session: SessionErrorsReducer,
+    
 });
