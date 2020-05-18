21bf19f2bf41eb7dfb81005fbbfde7db4a89573c
Tarik <tariksnow37@gmail.com>
2020-05-18 15:51:29 -0700

    correct format for blog post in global state

--- frontend/src/reducers/blog_reducer.js
+++ frontend/src/reducers/blog_reducer.js
@@ -3,13 +3,17 @@ import { RECEIVE_BLOGS, RECEIVE_NEW_BLOG } from '../actions/blog_actions';
 const BlogsReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
-
+    
     switch(action.type) {
         case RECEIVE_NEW_BLOG:
             nextState[action.blog.title] = action.blog;
             return nextState;
         case RECEIVE_BLOGS:
-            nextState['blogs'] = action.blogs
+            let data = action.data.data
+            for(let i = 0; i < data.length; i++) {
+                nextState[i] = data[i]
+            }
+            return nextState;
         default:
             return nextState;
     };
