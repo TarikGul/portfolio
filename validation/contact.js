7758ca10e475ca81f7784b86828717a0cabf7f6f
Tarik <tariksnow37@gmail.com>
2020-05-26 15:11:26 -0700

    boiler validator for contacts

---
+++ validation/contact.js
@@ -0,0 +1,9 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateContactInput(contactData) {
+    const errors = {};
+    const data = { ...contactData };
+
+    
+}
