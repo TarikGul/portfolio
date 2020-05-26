6f02ea800c03a470e144ea6c8556626343580556
Tarik <tariksnow37@gmail.com>
2020-05-26 15:44:30 -0700

    post route for email contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,27 @@
 const express = require('express');
 
 const Contact = require('../../models/Contact');
+const validateContactInput = require('../../validation/contact');
 
 const router = express.Router();
 
 router.get('/test', (req, res) => res.json({ msg: 'This is a test route' }));
+
+router.post('/contact', (req, res) => {
+    const { body } = req;
+    const { errors, isValid } = validateContactInput(body);
+
+    if (!isValid) {
+        return res.status(400).json(errors)
+    };
+
+    const newContact = new Contact({
+        title: body.title,
+        message: body.message,
+        email: body.email
+    }) 
+
+    newContact.save()
+        .then((contact) => res.json(contact))
+        .catch(err => res.json(err))
+});
