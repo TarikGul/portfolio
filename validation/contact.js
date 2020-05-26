8b7f969ff7e7b77b09437e7e7f274f81eeac7b56
Tarik <tariksnow37@gmail.com>
2020-05-26 15:14:05 -0700

    validators

--- validation/contact.js
+++ validation/contact.js
@@ -5,5 +5,7 @@ module.exports = function validateContactInput(contactData) {
     const errors = {};
     const data = { ...contactData };
 
-    
+    data.body = validText(data.message) ? data.message : '';
+    data.title = validText(data.title) ? data.title : '';
+     
 }
