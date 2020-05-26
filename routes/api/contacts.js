dd5fcb51606be59c041015fff6a32530f1345bc1
Tarik <tariksnow37@gmail.com>
2020-05-26 15:28:22 -0700

    require Contact

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const Contact = require('../../models/Contact');
+
 const router = express.Router();
 
 router.get('/test', (req, res) => res.json({ msg: 'This is a test route' }));
