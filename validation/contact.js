f6ef1d47a8d46f7d8b78a022d061aa65acbfb40e
Tarik <tariksnow37@gmail.com>
2020-05-26 15:22:12 -0700

    email validator

--- validation/contact.js
+++ validation/contact.js
@@ -7,5 +7,22 @@ module.exports = function validateContactInput(contactData) {
 
     data.body = validText(data.message) ? data.message : '';
     data.title = validText(data.title) ? data.title : '';
-     
-}
+    data.email = validText(data.email) ? data.email : '';
+    
+    if (Validator.isEmpty(data.message)) {
+        errors.message = 'Message field must be filled';
+    };
+
+    if (Validator.isEmpty(data.title)) {
+        errors.title = 'Title field must be filled';
+    };
+
+    if (Validator.isEmpty(data.email)) {
+        errors.email = 'Email field must be filled';
+    };
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    };
+};
