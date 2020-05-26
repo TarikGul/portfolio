2a64c9c1b207e34ae7c209a185e1c78ff60df497
Tarik <tariksnow37@gmail.com>
2020-05-26 15:05:34 -0700

    contact model object

---
+++ models/Contact.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ContactSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Contact = mongoose.model('Contact', ContactSchema);
+module.exports = Contact;
