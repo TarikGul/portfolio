751c78e9a442cfe21bf5e745bda445a7cca81023
Tarik <tariksnow37@gmail.com>
2020-05-27 15:04:52 -0700

    resolve bug in blogs global state

--- frontend/src/reducers/blog_reducer.js
+++ frontend/src/reducers/blog_reducer.js
@@ -3,10 +3,11 @@ import { RECEIVE_BLOGS, RECEIVE_NEW_BLOG } from '../actions/blog_actions';
 const BlogsReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
-    
+
     switch(action.type) {
         case RECEIVE_NEW_BLOG:
-            nextState[action.data.data.title] = action.blog;
+            let data = action.data.data
+            nextState[state.length] = data;
             return nextState;
         case RECEIVE_BLOGS:
             let data = action.data.data
