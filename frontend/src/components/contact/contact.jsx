478af00e8a1f8be29a8bc4dd0d9842f019a0db5e
Tarik <tariksnow37@gmail.com>
2020-05-26 20:03:24 -0700

    give Contact state

--- frontend/src/components/contact/contact.jsx
+++ frontend/src/components/contact/contact.jsx
@@ -5,13 +5,21 @@ class Contact {
     constructor(props) {
         super(props)
 
+        this.state = {
+            title: '',
+            message: '',
+            email: '',
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(e) {
         e.preventDefault();
 
+
     }
-    
+
     render() {
         return (
             <div className='contact-container'>
