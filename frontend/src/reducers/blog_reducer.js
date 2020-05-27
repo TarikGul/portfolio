db0161524b30c0657b05fceedc0970d6db038c00
Tarik <tariksnow37@gmail.com>
2020-05-27 15:05:41 -0700

    cleanup code

--- frontend/src/reducers/blog_reducer.js
+++ frontend/src/reducers/blog_reducer.js
@@ -3,14 +3,14 @@ import { RECEIVE_BLOGS, RECEIVE_NEW_BLOG } from '../actions/blog_actions';
 const BlogsReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
-
+    let data;
     switch(action.type) {
         case RECEIVE_NEW_BLOG:
-            let data = action.data.data
+            data = action.data.data
             nextState[state.length] = data;
             return nextState;
         case RECEIVE_BLOGS:
-            let data = action.data.data
+            data = action.data.data
             for(let i = 0; i < data.length; i++) {
                 nextState[i] = data[i]
             }
