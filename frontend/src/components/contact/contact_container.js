50aa8d3b8ae75cee85143f943f2c66ae180aaa74
Tarik <tariksnow37@gmail.com>
2020-05-26 17:56:53 -0700

    dispatch createContact

--- frontend/src/components/contact/contact_container.js
+++ frontend/src/components/contact/contact_container.js
@@ -1,16 +1,17 @@
 import { connect } from 'react-redux';
 
+import { createContact } from '../../util/contact_util';
 import Contact from './contact';
 
 const msp = state => {
     return {
-
+        
     };
 };
 
 const mdtp = dispatch => {
     return {
-        
+        createContact: data => dispatch(createContact(data))
     };
 };
 
