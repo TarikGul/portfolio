72f97dfa435e0e139023c502df122eb3e0135a3d
Tarik <tariksnow37@gmail.com>
2020-05-27 22:47:02 -0700

    functionality and styling

--- frontend/src/components/blog/blog_item.jsx
+++ frontend/src/components/blog/blog_item.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
+const parseDate = date => {
+    const split = date.split('T');
+    return split[0];
+}
+
 const BlogItem = props => {
     const { title, description, date } = props;
     return (
         <div className='blog-item-container'>
-            {title} <br/>
-            {description} <br/>
-            {date} <br/>
+            <div className='blog-title'>
+                {title}
+            </div>
+            <div className='blog-date'>
+                {parseDate(date)}
+            </div>
+            <div className='blog-description'>
+                {description}
+            </div>  
         </div>
     )
 };
