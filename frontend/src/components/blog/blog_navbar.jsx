5e500baa60083e6337f868412590f7d1e08d342a
Tarik <tariksnow37@gmail.com>
2020-05-27 20:46:44 -0700

    manage open close state for blog navbar

--- frontend/src/components/blog/blog_navbar.jsx
+++ frontend/src/components/blog/blog_navbar.jsx
@@ -4,12 +4,23 @@ class BlogNavbar extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+            open: false,
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+
+        this.setState(prevState => ({ open: !prevState }))
     }
 
     render() {
         return (
             <div className='blog-navbar-container'>
-                <div href='#' className='blog-navbar'>
+                <div href='#' className='blog-navbar' onClick={this.handleClick}>
                     <div className='one'></div>
                     <div className='two'></div>
                     <div className='three'></div>
