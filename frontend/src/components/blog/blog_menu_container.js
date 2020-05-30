533985b15cfcbfe270eb689df29ed4948c6cf7e2
Tarik <tariksnow37@gmail.com>
2020-05-30 14:44:36 -0700

    blog navbar boilerplate

---
+++ frontend/src/components/blog/blog_menu_container.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import BlogMenu from './blog_menu';
+
+const msp = state => {
+    return {
+
+    };
+};
+
+const mdtp = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(
+    msp,
+    mdtp
+)(BlogMenu);
