3111752ffd6dd516bdd49966c3cd178507d4c59a
Tarik <tariksnow37@gmail.com>
2020-05-31 05:38:42 -0700

    add fields validator

--- validation/blog.js
+++ validation/blog.js
@@ -7,13 +7,23 @@ module.exports = function validateBlogInput(blogData) {
 
     data.body = validText(data.description) ? data.description : '';
     data.title = validText(data.title) ? data.title : '';
+    data.quote = validText(data.quote) ? data.quote : '';
+    data.authorQuote = validText(data.authorQuote) ? data.authorQuote : '';
 
     if (Validator.isEmpty(data.description)) {
         errors.description = 'Description field must be filled';
     };
 
     if (Validator.isEmpty(data.title)) {
-        errors.description = 'Title field must be filled';
+        errors.title = 'Title field must be filled';
+    };
+
+    if (Validator.isEmpty(data.quote)) {
+        errors.quote = 'Quote field must be filled';
+    };
+
+    if (Validator.isEmpty(data.authorQuote)) {
+        errors.authorQuote = 'Author Quote field must be filled';
     };
 
     return {
