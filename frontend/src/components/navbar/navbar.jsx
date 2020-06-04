f02e4440253ad1f22153a18ddeef4999cca0140b
Tarik <tariksnow37@gmail.com>
2020-06-04 08:58:43 -0700

    fix the giithub logo

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -69,7 +69,7 @@ class Navbar extends React.Component {
                                 null
                             )
                     }
-                    <img className='github-logo' src='/github-logo.svg' width='45' height='45' onClick={() => this.reRoute('github')}/>
+                    <img className='github-logo' src='/github.svg' width='43' height='43' onClick={() => this.reRoute('github')}/>
                     <ResumeButton />
                     {/* <div className='light-toggle'>
                         <img src='/moon.svg' className='moon-svg'/>
