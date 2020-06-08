6f1ebcf33a938d9d7ff879c854854ec0577a5ac1
Tarik <tariksnow37@gmail.com>
2020-06-08 11:28:59 -0700

    transfer location saving to message server

--- routes/api/message_server.js
+++ routes/api/message_server.js
@@ -3,6 +3,10 @@ const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+// Used for saving the users location
+const Location = require('../../models/Location');
+const validateLocationInput = require('../../validation/location');
+
 const app = express();
 
 // Success webhook route for receiving messages,
@@ -10,10 +14,33 @@ const app = express();
 app.post('/sms', (req, res) => {
     const twiml = new MessagingResponse();
 
-    twiml.message('Your lat/long was successfully saved in the database');
+    const { body } = req;
+    const { errors, isValid } = validateLocationInput(body);
 
-    res.writeHead(200, { 'content-type': 'text/xml' });
-    res.end(twiml.toString());
+    if (!isValid) {
+        return res.status(400).json(errors);
+    };
+
+    const newLocation = new Location({
+        location: body.body
+    });
+
+    newLocation.save()
+        .then((location) => {
+            res.json(location);
+            twiml.message('Your lat/long was successfully saved in the database');
+
+            res.writeHead(200, { 'content-type': 'text/xml' });
+            res.end(twiml.toString());
+        })
+        .catch((err) => {
+            res.json(err)
+
+            twiml.message('Your request was unsuccesful');
+
+            res.writeHead(400, { 'content-type': 'text/xml' });
+            res.end(twiml.toString());
+        })
 });
 
 http.createServer(app).listen(1337, () => {
