d4501f6ef93624da0e7cee34580d2417eae38f86
Tarik <tariksnow37@gmail.com>
2020-06-08 11:03:46 -0700

    add test route

--- routes/api/location.js
+++ routes/api/location.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
+const Location = require('../../models/Location');
 const validateLocationInput = require('../../validation/location');
 
-
-
-
+router.get('/test', (req, res) => res.json({ msg: 'This is a test route' }));
