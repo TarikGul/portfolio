a661ae08d12fd503cec037b2915216de1deeb9f2
Tarik <tariksnow37@gmail.com>
2020-06-09 17:13:54 -0700

    communicate internally between routes and servers

--- routes/api/message_server.js
+++ routes/api/message_server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
 
+const querystring = require('querystring');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,17 +17,53 @@ app.post('/message', (req, res) => {
     const twiml = new MessagingResponse();
     
     const { body } = req;
+    console.log(body)
+    const postData = querystring.stringify({
+        location: body.Body
+    });
 
-    axios.post('/locations/location', body.Body)
-        .then((res) => {
-            twiml.message(`Your lat/long was successfully received from ${body.From}, and saved in the DB`);
-            console.log('success', res);
-        })
-        .catch((err) => {
-            twiml.message(`There was an errors in saving the lat/long in the database: ${err}`);
+    let host = (process.env.NODE_ENV === 'production') ? 'www.tarikgul.com' : 'localhost';
+
+    const options = {
+        hostname: host,
+        port: 5000,
+        path: '/api/locations/location',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+
+    const internalRequest = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
         });
-    
-    
+        res.on('end', () => {
+            twiml.message(`Your lat/long was successfully received from ${body.From}, and saved in the DB`);
+            console.log('No more data in response.');
+        });
+    });
+
+    internalRequest.on('error', (e) => {
+        twiml.message(`There was an errors in saving the lat/long in the database: ${e.message}`);
+        console.error(`problem with request: ${e.message}`);
+    });
+
+    // Write data to request body
+    internalRequest.write(postData);
+    internalRequest.end();
+    // if (err) {
+    //     twiml.message(`There was an errors in saving the lat/long in the database: ${err}`);
+    //     console.log(err)
+    // } else {
+    //     twiml.message(`Your lat/long was successfully received from ${body.From}, and saved in the DB`);
+    //     console.log(res)
+    // }
+ 
     res.writeHead(200, { 'content-type': 'text/xml' });
     res.end(twiml.toString());
 });
