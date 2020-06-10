7fac077ecc5b18ddb3f7832d0e09585c0f8b0031
Tarik <tariksnow37@gmail.com>
2020-06-10 14:35:56 -0700

    add config var for twilio api key

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -8,5 +8,6 @@ module.exports = {
     twilioAccountSid: process.env.TWILIO_ACCOUNT_SID,
     twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
     twilioPhoneNumber: process.env.TWILIO_PHONE_NUMBER,
-    personalPhone: process.env.PERSONAL_PHONE
+    personalPhone: process.env.PERSONAL_PHONE,
+    twilioAPIKey: process.env.TWILIO_API_KEY
 };
