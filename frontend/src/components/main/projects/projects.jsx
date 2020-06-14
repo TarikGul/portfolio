7d7ce0ed5dea2d85b1159378cb99189bfdac0b01
Tarik <tariksnow37@gmail.com>
2020-06-14 14:53:19 -0700

    new templates for projects

--- frontend/src/components/main/projects/projects.jsx
+++ frontend/src/components/main/projects/projects.jsx
@@ -14,35 +14,60 @@ const Projects = props => {
                 </div>
                 <div className='projects-boxes'>
                     <div className='box-container'>
-                        <span>Bimeo</span>
                         <img src='/bimeo-page.png' 
                              className='image-project'
                              onClick={() => handleReroute('https://bimeo.herokuapp.com')}/>
-                        <div className='project-description-container'>
-                            <div className='project-description'>
-                                Fullstack clone of Vimeo. Built on Ruby on Rails, React, Redux, webpack, PostgreSQL. Displays full CRUD actions, RESTful API routes, and database management. 
+                        <div className='project-right-box'>
+                            <div className='project-name'>
+                                Bimeo
+                            </div>
+                            <div className='project-description-container'>
+                                <div className='project-description'>
+                                    Fullstack clone of Vimeo. Built on Ruby on Rails, React, Redux, webpack, PostgreSQL. Displays full CRUD actions, RESTful API routes, and database management. 
+                                </div>
+                            </div>
+                            <div className='technologies'>
+                                <div className='technologies-header'>
+                                    Technologies
+                                </div>
+                                <span>React</span>
+                                <span>Redux</span>
                             </div>
                         </div>
                     </div>
                     <div className='box-container'>
-                        <span>Covid-415</span>
                         <img src='/covid-415.png' 
                              className='image-project'
                              onClick={() => handleReroute('https://covid415.herokuapp.com')}/>
-                        <div className='project-description-container'>
-                            <div className='project-description'>
-                                Built using the MERN stack, COVID415 gives San Franciscan's the resources to establish a connection for help during the coronavirus pandemic.
+                        <div className='project-right-box'>
+                            <div className='project-name'>
+                                Covid-415
+                            </div>
+                            <div className='project-description-container'>
+                                <div className='project-description'>
+                                    Built using the MERN stack, COVID415 gives San Franciscan's the resources to establish a connection for help during the coronavirus pandemic.
+                                </div>
+                            </div>
+                            <div className='technologies'>
+
                             </div>
                         </div>
                     </div>
                     <div className='box-container'>
-                        <span>Sudoku-Visualizer</span>
                         <img src='sudoku-solver.png' 
                              className='image-project'
                              onClick={() => handleReroute('https://tarikgul.github.io/Sudoku-solving-visualizer/dist/')}/>
-                        <div className='project-description-container'>
-                            <div className='project-description'>
-                                This is a Sudoku solving visualizer. It focus's on solving the Exact Cover problem. Used in computer science and mathematics it is studied under the NP vs P debate.
+                        <div className='project-right-box'>
+                            <div className='project-name'>
+                                Sudoku Solver
+                            </div>
+                            <div className='project-description-container'>
+                                <div className='project-description'>
+                                    This is a Sudoku solving visualizer. It focus's on solving the Exact Cover problem. Used in computer science and mathematics it is studied under the NP vs P debate.
+                                </div>
+                            </div>
+                            <div className='technologies'>
+
                             </div>
                         </div>
                     </div>
