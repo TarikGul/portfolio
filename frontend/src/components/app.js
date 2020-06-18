46d1d5370f4a2cbd090574aa647e0c9165296b1b
Tarik <tariksnow37@gmail.com>
2020-06-18 08:53:43 -0700

    react-ga

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import { Route } from 'react-router-dom';
 
 import { AuthRoute } from '../util/route_util';
@@ -10,6 +11,11 @@ import ContactContainer from './contact/contact_container';
 import LoginFormContainer from './session/login_form_container';
 import BlogContainer from './blog/blog_container';
 import Modal from './modal/modal';
+
+if (window.location.hostname !== 'localhost') {
+    ReactGA.initialize('UA-162754702-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 const App = () => (
     <div>
