0ec08649ddbd9d75b95da70b08a3c88f3c6fddc6
Tarik <tariksnow37@gmail.com>
2020-06-24 18:19:05 -0400

    add web.js

---
+++ web.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+// write nginx tmp
+fs.writeFile("/tmp/app-initialized", "Ready to launch nginx", function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("The file was saved!");
+    }
+});
+
+// listen on the nginx socket
+app.listen('/tmp/nginx.socket', function () {
+    console.log("Listening ");
+});
+
