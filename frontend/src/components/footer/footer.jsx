084fbfba95f2b6a1e16d76e72c5d03b278b84672
Tarik <tariksnow37@gmail.com>
2020-06-25 03:11:28 -0400

    position prop added

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import '../../styles/footer.scss';
 
 const Footer = props => {
+
+    const styles = {
+        position: props.position
+    };
+
+    const position = styles.position;
+
     return (
-        <div className='footer-wrapper'>
+        <div className='footer-wrapper' style={{ position }}>
             <p className='footer-quote'> "The queiter you become, the more you can hear" <br/>
             -Some Hacker
             </p>
