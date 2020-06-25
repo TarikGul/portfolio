c426f79201a8d73df22fd0d82f7ad590ad6d809e
Tarik <tariksnow37@gmail.com>
2020-06-25 02:02:32 -0400

    add styling to footer and p tag

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../../styles/footer.scss';
 
 const Footer = props => {
     return (
         <div className='footer-wrapper'>
-            "The queiter you become, the more you can hear" <br/>
+            <p className='footer-quote'> "The queiter you become, the more you can hear" <br/>
             -Some Hacker
+            </p>
         </div>
     )
 }
