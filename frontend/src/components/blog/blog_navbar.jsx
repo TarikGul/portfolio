b6435461530ac72ec2c58ba07e616e6394d6563d
Tarik <tariksnow37@gmail.com>
2020-06-29 21:23:00 -0400

    blogNavbar to functional component using hooks

--- frontend/src/components/blog/blog_navbar.jsx
+++ frontend/src/components/blog/blog_navbar.jsx
@@ -1,42 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BlogNavIcon from './navbar_icon';
 
-class BlogNavbar extends React.Component {
-    constructor(props) {
-        super(props)
+const BlogNavbar = (props) => {
+    const [open, setOpen] = useState(false);
 
-        this.state = {
-            open: false
-        }
+    const handleClick = () => {
+        const newState = !open;
+        setOpen(newState);
+    };
 
-        // this.wrapperRef = React.createRef();
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(e) {
-        e.preventDefault();
-        console.log('hello')
-
-        this.setState(prevState => ({ open: !prevState.open }))
-        console.log(this.state)
-        // const wrapper = this.wrapperRef.current;
-        // wrapper.classList.toggle('is-nav-open');
-    }
-
-    render() {
-
-        const { open } = this.state;
-        return (
-            <div className='blog-navbar-container'>
-                <div className='wrapper'>
-                    {
-                        open ? 
+    return (
+        <div className='blog-navbar-container'>
+            <div className='wrapper'>
+                {
+                    open ?
                         (
                             <div className='nav-open'>
                                 <BlogNavIcon
                                     open={true}
                                     type='menu-fold'
-                                    handleClick={this.handleClick}
+                                    handleClick={() => handleClick()}
                                 />
                                 <div className='nav-body'>
                                     <div>
@@ -48,12 +31,12 @@ class BlogNavbar extends React.Component {
                                 </div>
                             </div>
                         ) : (
-                            
+
                             <div className='nav'>
-                                <BlogNavIcon 
+                                <BlogNavIcon
                                     open={false}
                                     type='menu-fold'
-                                    handleClick={this.handleClick}
+                                    handleClick={() => handleClick()}
                                 />
                                 <div className='nav-body-closed'>
                                     <div>
@@ -65,11 +48,10 @@ class BlogNavbar extends React.Component {
                                 </div>
                             </div>
                         )
-                    }
-                </div>
+                }
             </div>
-        )
-    }
+        </div>
+    );
 }
 
 export default BlogNavbar;
