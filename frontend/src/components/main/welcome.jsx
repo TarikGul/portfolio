1a8a007d0a954a108e78844678f5e1fa349e20a5
Tarik <tariksnow37@gmail.com>
2020-06-29 22:14:01 -0400

    change welcome from class to functional component

--- frontend/src/components/main/welcome.jsx
+++ frontend/src/components/main/welcome.jsx
@@ -1,32 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Prism from '../../../node_modules/prismjs';
 import '../../styles/prism.scss';
 import '../../styles/welcome.scss'
 
-class Welcome extends React.Component {
-    constructor(props) {
-        super(props)
-
-    }
-
-    componentDidMount() {
+const Welcome = props => {
+    useEffect(() => {
         Prism.highlightAll();
-    }
+    }, []);
 
-    render() {
-        return (
-            <div className="welcome-container">
-                <div className="border_gradient">
-                    <img src="/self-pic.png" className="self-pic"/>
+    return (
+        <div className="welcome-container">
+            <div className="border_gradient">
+                <img src="/self-pic.png" className="self-pic" />
+            </div>
+            <div className="welcome-pizzaz">
+                <div className="name-welcome">
+                    TARIK GUL
                 </div>
-                <div className="welcome-pizzaz">
-                    <div className="name-welcome">
-                        TARIK GUL
-                    </div>
-                    {/* <div className="code-snippet"> */}
-                        <pre >
-                            <code className="language-javascript">
-                                {`
+                <pre >
+                    <code className="language-javascript">
+                        {`
 onSubmit(e) {
     e.preventDefault();
     this.setState(prevState => {
@@ -35,13 +28,11 @@ onSubmit(e) {
     console.log('Welcome to my personal website! One love')
 }
                                 `}
-                            </code>
-                        </pre>
-                    {/* </div> */}
-                </div>
+                    </code>
+                </pre>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Welcome;
