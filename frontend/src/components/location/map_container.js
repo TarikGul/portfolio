bfa9e2527af1257213168ccc70624a2ecca9eaa3
Tarik <tariksnow37@gmail.com>
2020-07-13 17:32:53 -0400

    pass adventures into props

--- frontend/src/components/location/map_container.js
+++ frontend/src/components/location/map_container.js
@@ -1,18 +1,21 @@
 import { connect } from 'react-redux';
 
 import { fetchLocations } from '../../actions/location_actions';
+import { fetchGeojson } from '../../actions/adventures_actions';
 import Map from './map';
 
 const msp = state => {
-    const { errors, location } = state;
+    const { errors, location, adventures } = state;
     return {
         errors,
-        location
+        location,
+        adventures
     };
 };
 
 const mdtp = dispatch => {
     return {
+        fetchGeojson: () => dispatch(fetchGeojson()),
         fetchLocations: () => dispatch(fetchLocations())
     };
 };
