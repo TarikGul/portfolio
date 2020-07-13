abc74500c1ea45da8afe0bdfa9236fc0db20b2a0
Tarik <tariksnow37@gmail.com>
2020-07-13 17:34:29 -0400

    add token for verification to receive files

--- routes/api/adventures.js
+++ routes/api/adventures.js
@@ -9,12 +9,8 @@ router.get('/test', (req, res) => res.json({ msg: 'This is a test' }));
 
 router.get('/index', (req, res) => {
     if(req.authKey === keys.trailAuth) {
-        console.log('hit this route')
-        console.log(mongoose);
-        console.log(Inventory)
         Inventory.find()
             .then((files) => {
-                console.log(files)
                 res.json(files)
             })
             .catch((err) => res.status(404).json({ notasksfound: 'No trails found', err }));
