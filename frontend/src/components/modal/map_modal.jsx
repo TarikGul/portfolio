c110bb267dc110ac40ac7dfe0865a26a5758a4ce
Tarik <tariksnow37@gmail.com>
2020-07-15 01:42:46 -0400

    add modals for the maps

---
+++ frontend/src/components/modal/map_modal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import data from '../../util/map_trails_data';
+
+const MapModal = (props) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MapModal;
---
+++ frontend/src/components/modal/map_modal_container.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+
+import MapModal from './map_modal';
+
+const msp = (state) => {
+    return {
+
+    }
+}
+
+const mdtp = (dispatch) => {
+    return {
+
+    }
+}
+
+export default connect(
+    msp,
+    mdtp
+)(MapModal);
