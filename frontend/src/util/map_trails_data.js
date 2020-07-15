36816c4863b25baa3c284866039f8dc14f14ee75
Tarik <tariksnow37@gmail.com>
2020-07-15 01:42:23 -0400

    add trail data for the maps

---
+++ frontend/src/util/map_trails_data.js
@@ -0,0 +1,22 @@
+module.exports ={
+    'routes': {
+        'CDT': {
+            'name': 'The Continental Divide Trail',
+            'miles': '3100',
+            'details': 'The CDT, or more popularly known as The Continental Divide Trail is a 3100 mile continous foot path from the border of Mexico, and New Mexico, to Canada and Montana. It passes through states such as New Mexico, Colorado, Wyoming, Idaho, and Montana. Back in 2018 i started at Mexico, and walked it up to Canada. Over the 94 days it took me to walk it, I saw some of the most magical things i could imagine.',
+            'states': ['New Mexico', 'Colorado', 'Wyoming', 'Idaho', 'Montana']
+        },
+        'PCT': {
+            'name': 'The Pacific Crest Trail',
+            'miles': '2652',
+            'details': '',
+            'states': ['California', 'Oregon', 'Washington']
+        },
+        'CCT': {
+            'name': 'The California Coastal Trail',
+            'miles': '1200',
+            'details': '',
+            'states': ['California']
+        }
+    }
+}
