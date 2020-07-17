a5170cb7a0d098031b76d539eba9269d175bf158
Tarik <tariksnow37@gmail.com>
2020-07-17 18:49:15 -0400

    catch err

--- routes/api/adventures.js
+++ routes/api/adventures.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 
 const Inventory = require('../../models/Inventory');
 const keys = require('../../config/keys');
@@ -13,7 +12,7 @@ router.post('/index', (req, res) => {
             .then((files) => {
                 res.json(files)
             })
-            .catch((err) => res.status(404).json({ notasksfound: 'No trails found', err }));
+            .catch((err) => res.status(404).json({ noTasksFound: 'No trails found', err }));
     } else {
         res.send('Good Try')
     }
