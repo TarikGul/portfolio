0fc731a1dc4fa65f969370ee21266b0b6b846e15
Tarik <tariksnow37@gmail.com>
2020-10-02 20:35:37 -0700

    add preloading and cached actions

--- frontend/src/reducers/adventure_reducer.js
+++ frontend/src/reducers/adventure_reducer.js
@@ -1,16 +1,37 @@
-import { RECEIVE_GEO_JSON } from '../actions/adventures_actions';
+import { 
+    RECEIVE_GEO_JSON, 
+    PRELOADING_GEO_JSON,
+    CACHED_GEO_JSON 
+} from '../actions/adventures_actions';
 
 const AdventuresReducer = (state = {}, action) => {
     Object.freeze(state);
     let nextState = Object.assign({}, state);
 
     let data;
+    let preloading;
+    let cached;
     switch(action.type) {
         case RECEIVE_GEO_JSON:
             data = action.data.data;
             nextState.geojson = data;
             return nextState;
+        case PRELOADING_GEO_JSON:
+            preloading = action.boolean;
+            nextState.preloading = preloading;
+            return nextState;
+        case CACHED_GEO_JSON:
+            cached = action.boolean;
+            nextState.cached = cached;
+            return nextState;
         default:
+            if(nextState.preloading === undefined) {
+                nextState.preloading = false;
+            }
+
+            if(nextState.cached === undefined) {
+                nextState.cached = false;
+            }
             return nextState;
     };
 }
