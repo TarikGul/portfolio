1364206e38afd948704dddb9e54c1357098d0245
Tarik <tariksnow37@gmail.com>
2020-10-02 20:36:21 -0700

    mdtp

--- frontend/src/components/main/main_page_container.js
+++ frontend/src/components/main/main_page_container.js
@@ -2,16 +2,25 @@ import { connect } from 'react-redux';
 
 import Main from './main_page';
 import { openModal } from '../../actions/modal_actions';
+import { 
+    fetchGeojson, 
+    setPreloadingGeojson, 
+    setCachedGeojson 
+} from '../../actions/adventures_actions';
 
 const msp = state => {
+    const { adventures } = state;
     return {
-
+        adventures
     };
 };
 
 const mdtp = dispatch => {
     return {
-        openModal: modal => dispatch(openModal(modal))
+        openModal: modal => dispatch(openModal(modal)),
+        fetchGeojson: (key) => dispatch(fetchGeojson(key)),
+        setPreloadingGeojson: (boolean) => dispatch(setPreloadingGeojson(boolean)),
+        setCachedGeojson: (boolean) => dispatch(setCachedGeojson(boolean))
     };
 };
 
