31d9366a2c994fcaf5c601c5e51d42cd1fda6147
Tarik <tariksnow37@gmail.com>
2020-10-02 20:36:00 -0700

    add preloading and cached actions

--- frontend/src/actions/adventures_actions.js
+++ frontend/src/actions/adventures_actions.js
@@ -2,6 +2,8 @@ import * as APIUTIL from '../util/adventures_util';
 
 export const RECEIVE_GEO_JSON = 'RECEIVE_GEO_JSON';
 export const RECEIVE_GEO_JSON_ERRORS = 'RECEIVE_GEO_JSON_ERRORS';
+export const PRELOADING_GEO_JSON = 'PRELOADING_GEO_JSON';
+export const CACHED_GEO_JSON = 'CACHED_GEO_JSON';
 
 const receiveGeojson = data => {
     return {
@@ -17,8 +19,32 @@ const receiveErrors = (errors) => {
     };
 };
 
+const preloadGeojson = boolean => {
+    return {
+        type: PRELOADING_GEO_JSON,
+        boolean
+    };
+};
+
+// This means that we called fetchGeojson somewhere and the geojson will be cached
+// in global state
+const cachedGeojson = boolean => {
+    return {
+        type: CACHED_GEO_JSON,
+        boolean
+    }
+}
+
 export const fetchGeojson = (key) => dispatch => {
     return APIUTIL.fetchGeojson(key)
         .then(files => dispatch(receiveGeojson(files)))
         .catch(err => dispatch(receiveErrors(err)))
+};
+
+export const setPreloadingGeojson = (boolean) => dispatch => {
+    return dispatch(preloadGeojson(boolean));
+};
+
+export const setCachedGeojson = (boolean) => dispatch => {
+    return dispatch(cachedGeojson(boolean));
 };
