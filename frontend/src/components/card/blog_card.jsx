4c96e88989de00ea12d40ba291f147db3d12742c
Tarik <tariksnow37@gmail.com>
2020-10-31 22:58:46 -0700

    blog routing, useHistory hook

--- frontend/src/components/card/blog_card.jsx
+++ frontend/src/components/card/blog_card.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const BlogCard = (props) => {
-    const { title, description, imageUrl } = props;
+    const history = useHistory();
+    const { title, description, imageUrl, url } = props;
+
+    const handleClick = () => {
+        history.push(`/blog/${url}`);
+    }
 
     return (
-        <div className='blog-card'>
+        <div className='blog-card' onClick={() => handleClick()}>
             <h2 className='blog-card-header'>{title}</h2>
             <div className='blog-card-container'>
                 <img className='blog-card-image' src={imageUrl}/>
